b0dfe92ea5e97ae77ed03ce10ab28677026255bf
greenzorro <greenzoro@163.com>
2014-07-12 02:35:26 +0800

    修改作品页面的图标与标题

--- js/lower-ie.js
+++ js/lower-ie.js
@@ -1,4 +1,4 @@
 $(function () {
 	$("body").addClass("bg");
-	$("body").html('<div class="explorer">您的浏览器过时，建议使用谷歌浏览器，火狐浏览器打开</div>');
+	$("body").html('<div class="explorer">您的浏览器已过时，建议使用谷歌浏览器或火狐浏览器打开</div>');
 })
