3e2b9f19d77ed54a259bdefb75b9b0df6e37dfa4
greenzorro <greenzoro@163.com>
2014-07-12 00:25:10 +0800

    页面框架完成，测试一下判断浏览器功能

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,8 @@ $(function () {
 	experience();  //首页个人经历时间轴
 
 	// 基础功能
+	anchorSlide();  //锚链接平滑移动
+	picCenter();  //居中截取图片
 
 	// 插件效果
 	fancybox();  //fancybox弹窗相册
@@ -34,6 +36,76 @@ function experience () {
 //*********************************************************************//
 //                              基础功能                               //
 //*********************************************************************//
+
+
+// 锚链接平滑移动
+function anchorSlide () {
+	$("a[href*='#']").click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var $target = $(this.hash);
+			$target = $target.length && $target || $("[name=" + this.hash.slice(1) + "]");
+			if (this.hash.slice(1)){
+				if($target.length){
+					var targetOffset = $target.offset().top;
+					$("html,body").animate({
+						scrollTop: targetOffset
+					},
+					300);
+				}
+			}
+			else{
+				$("html,body").animate({
+					scrollTop: 0
+				},
+				300);
+			}
+			return false;  //防止页面跳动
+		}
+	});	
+}
+
+// 居中截取图片
+function picCenter () {
+	if($(".pic_center").length > 0)
+	{
+		setAllCenter();
+	}
+	function setImg(thisEle){
+		var wrapWidth = $(thisEle).width();
+		var wrapHeight = $(thisEle).height();
+		var picWidth = $(thisEle).find("img").width();
+		var picHeight = $(thisEle).find("img").height();
+		var wrapShape = wrapWidth / wrapHeight;
+		var picShape = picWidth / picHeight;
+		var picWidthS = picWidth/picHeight*wrapHeight;
+		var picHeightS = picHeight/picWidth*wrapWidth;
+		if(wrapShape > picShape)
+		{
+			$(thisEle).find("img").css({"width":"100%","height":"auto","top":(wrapHeight-picHeightS)/2,"left":"0"});
+		}
+		else
+		{
+			$(thisEle).find("img").css({"width":"auto","height":"100%","left":(wrapWidth-picWidthS)/2,"top":"0"});
+		}
+	}
+	function setAllCenter(){
+		$(".pic_center").each(function(){
+			var self = this;
+			var pic = $(self).find("img");
+			var src = $(this).attr("src");
+			$(pic).hide();
+			var imgW,imgH;
+			var img = new Image();
+			$(img).bind("load", function() {
+				imgW = pic.width();
+				imgH = pic.height();
+				setImg(self);
+			}).attr("src", pic.attr("src"));
+			pic.show()
+		});
+	}
+}
+
 
 
 
---
+++ js/lower-ie.js
@@ -0,0 +1,4 @@
+$(function () {
+	$("body").addClass("bg");
+	$("body").html('<div class="explorer">您的浏览器过时，建议使用谷歌浏览器，火狐浏览器打开</div>');
+})
